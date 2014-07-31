84102949f9527d91f4bdb120b13e153cd8499368
Ali Fathalian <fatalian@uw.edu>
2014-07-30 20:46:42 -0700

    Question 9 + Unit Tests

---
+++ JavaSolutions/src/main/java/com/hackergym/samples/array/questions/PalindromeSubstring.java
@@ -0,0 +1,112 @@
+package com.hackergym.samples.array.questions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Fathalian on 7/30/14.
+ * HackerGym.com
+ */
+public class PalindromeSubstring {
+
+    public Set<String> findPalindromSubstringsEnumeration(String s) {
+
+        Set<String> result = new HashSet<>();
+        Set<String> allSubStrings = findAllSubstrings(s);
+        for (String subString : allSubStrings) {
+            //we don't consider signle character strings palindromes in this problem
+            if (isPalindrome(subString) && subString.length() != 1) {
+                result.add(subString);
+            }
+        }
+
+        return result;
+    }
+
+    private Set<String> findAllSubstrings(String s) {
+
+        //using a set data structure will cause us not to include duplicates
+        Set<String> allSubStrings = new HashSet<>();
+
+        //although we could use the substring methods provided on
+        //the String object itself, we are going to do it the hard way and
+        //use a char array for educational purposes.In the real world, you should
+        //always prefer a well tested native method to rolling something your own
+        char[] sChars = s.toCharArray();
+        //start from index 0 and generate all the substrings
+        for (int i = 0; i < sChars.length; i++) {
+
+            //generate all the substrings from length i to length sChars.length
+            for (int substringStartIndex = i; substringStartIndex < sChars.length; substringStartIndex++) {
+
+                //create the substring from the chars and append it to the result
+                StringBuilder builder = new StringBuilder();
+                for (int sliceIndex = i; sliceIndex <= substringStartIndex; sliceIndex++) {
+                    builder.append(sChars[sliceIndex]);
+                }
+                allSubStrings.add(builder.toString());
+            }
+        }
+
+        return allSubStrings;
+    }
+
+    private boolean isPalindrome(String s) {
+
+        char[] charArray = s.toCharArray();
+        int beginning = 0;
+        int end = s.length() - 1;
+        while (beginning <= end) {
+            if (charArray[beginning] != charArray[end]) {
+                return false;
+            }
+            beginning++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public Set<String> findPalindromeSubstringsGeneration(String s) {
+
+        //using a set data structure will cause us not to include duplicates
+        Set<String> result = new HashSet<>();
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            //for even sized palindromes,
+            //Start the check from the element itself and its right neighbor
+            expandForPalindromes(i, i + 1, charArray, result);
+            //for odd sized palindromes,
+            //ignore the current element and start the check from the
+            //elements to its left and right
+            expandForPalindromes(i - 1, i + 1, charArray, result);
+
+        }
+        return result;
+    }
+
+    private void expandForPalindromes(int leftIndex,
+                                      int rightIndex,
+                                      char[] array,
+                                      Set<String> results) {
+
+
+        StringBuilder builder = new StringBuilder();
+        //for odd sized substrings we first add the middle string to
+        //the String builder
+        if (leftIndex == rightIndex -2 &&
+                leftIndex > 0) {
+            builder.append(array[leftIndex + 1]);
+        }
+        while (leftIndex >= 0 && rightIndex < array.length) {
+            if (array[leftIndex] != array[rightIndex]) {
+                break;
+            }
+            builder.insert(0, array[leftIndex]).append(array[rightIndex]);
+            results.add(builder.toString());
+            leftIndex--;
+            rightIndex++;
+        }
+
+    }
+}
---
+++ JavaSolutions/src/test/java/com/hackergym/samples/array/questions/PalindromeSubstringTest.java
@@ -0,0 +1,82 @@
+package com.hackergym.samples.array.questions;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class PalindromeSubstringTest {
+
+    PalindromeSubstring palindromeFinder = new PalindromeSubstring();
+    @Test
+    public void testFindPalindromSubstringsEnumerationEvenSize() throws Exception {
+        String s = "aabbaa";
+        Set<String> expectedResult = new HashSet<>(Arrays.asList("aa","bb", "abba",  "aabbaa"));
+        Set<String> actualResult = palindromeFinder.findPalindromSubstringsEnumeration(s);
+
+        assertThat(actualResult, is(expectedResult));
+    }
+
+    @Test
+    public void testFindPalindromSubstringsEnumerationOddSize() throws Exception {
+        String s = "aabbbaa";
+        Set<String> expectedResult = new HashSet(Arrays.asList("aa","bb", "bbb", "abbba",  "aabbbaa"));
+        Set<String> actualResult = palindromeFinder.findPalindromSubstringsEnumeration(s);
+
+        assertThat(expectedResult, is(actualResult));
+    }
+
+    @Test
+    public void testFindPalindromSubstringsEnumerationNoPalindrome() throws Exception {
+
+        String s = "abcde";
+        Set<String> actualResult = palindromeFinder.findPalindromSubstringsEnumeration(s);
+
+        assertThat(actualResult.size(), is(0));
+    }
+
+    @Test
+    public void testFindPalindromeSubstringsEnumerationEmptyString() throws Exception {
+        String s = "";
+        Set<String> actualResult = palindromeFinder.findPalindromSubstringsEnumeration(s);
+
+        assertThat(actualResult.size(), is(0));
+    }
+
+    @Test
+    public void testFindPalindromeSubstringsGenerationEvenSize() throws Exception {
+        String s = "aabbaa";
+        Set<String> expectedResult = new HashSet<>(Arrays.asList("aa","bb", "abba",  "aabbaa"));
+        Set<String> actualResult = palindromeFinder.findPalindromeSubstringsGeneration(s);
+
+        assertThat(expectedResult, is(actualResult));
+    }
+
+    @Test
+    public void testFindPalindromeSubstringsGenerationOddSize() throws Exception {
+        String s = "aabbbaa";
+        Set<String> expectedResult = new HashSet<>(Arrays.asList("aa","bb", "bbb", "abbba",  "aabbbaa"));
+        Set<String> actualResult = palindromeFinder.findPalindromeSubstringsGeneration(s);
+
+        assertThat(expectedResult, is(actualResult));
+    }
+
+    @Test
+    public void testFindPalindromeSubstringsGenerationNoPalindrome() throws Exception {
+        String s = "abcde";
+        Set<String> actualResult = palindromeFinder.findPalindromeSubstringsGeneration(s);
+
+        assertThat(actualResult.size(), is(0));
+    }
+
+    public void testFindPalindromeSubstringsGenerationEmptyString() throws Exception {
+        String s = "";
+        Set<String> actualResult = palindromeFinder.findPalindromeSubstringsGeneration(s);
+
+        assertThat(actualResult.size(), is(0));
+    }
+}
