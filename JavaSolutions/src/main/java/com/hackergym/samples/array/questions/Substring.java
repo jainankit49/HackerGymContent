f8cad93a242d87cdedb9b8a8196dafe530e4728e
Ali Fathalian <fatalian@uw.edu>
2014-08-07 14:19:01 -0700

    Question 21 Code + Test

---
+++ JavaSolutions/src/main/java/com/hackergym/samples/array/questions/Substring.java
@@ -0,0 +1,128 @@
+package com.hackergym.samples.array.questions;
+
+/**
+ * Created by Fathalian on 8/6/14.
+ * HackerGym.com
+ */
+public class Substring {
+
+    public int findSubstringBruteForce(String text, String search) {
+
+        if (search.length() > text.length()) return -1;
+
+
+        char[] textChars = text.toCharArray();
+        char[] searchChars = search.toCharArray();
+
+        for (int i = 0; i < textChars.length - searchChars.length + 1; i++) {
+            if (matchSubstring(textChars, i, searchChars)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //returns true that search appeared in text, starting from index "start"
+    private boolean matchSubstring(char[] text, int start, char[] search) {
+
+        int matchedLength = 0;
+        while (matchedLength < search.length) {
+            if (text[start + matchedLength] != search[matchedLength]) {
+                return false;
+            }
+            matchedLength++;
+        }
+        return true;
+    }
+
+
+    public int findSubstringRabinKarp(String text, String search) {
+
+        if (search.length() > text.length()) return -1;
+
+        char[] textChars = text.toCharArray();
+        char[] searchChars = search.toCharArray();
+
+        //calculate initial fingerprints
+        double searchFingerprint = calculateHash(searchChars, 0, search.length() - 1);
+        double rollingFingerprint = calculateHash(textChars, 0, search.length() - 1);
+
+        //to keep a simpler while loop we do the initial iteration.
+        //This is because the initial iteration does not need a hash.
+        boolean didFind = true;
+        if (rollingFingerprint == rollingFingerprint) {
+            for (int i = 0; i < search.length(); i++) {
+                if (searchChars[i] != textChars[i]) {
+                    didFind = false;
+                    break;
+                }
+                //we have found the answer
+            }
+            if (didFind) return 0;
+        }
+
+        int i = 1;
+        //we don't need to compare all of test with search
+        //after text.length() - searchChars + 1.length, there is no chance
+        //that search is in text.
+        while (i < textChars.length - searchChars.length + 1) {
+
+            //calculate the rolling finger print
+            //for the next section of the text array
+            rollingFingerprint = calculateRollingHash(
+                    rollingFingerprint,
+                    textChars,
+                    i + searchChars.length - 1,
+                    i - 1,
+                    searchChars.length);
+
+            //check for equality one by one if the fingerprints are the same
+            if (rollingFingerprint == searchFingerprint) {
+                didFind = true;
+                for (int j = 0; j < searchChars.length; j++) {
+                    if (textChars[i + j] != searchChars[j]) {
+                        didFind = false;
+                        break;
+                    }
+                }
+                //we have found the answer
+                if (didFind) return i;
+            }
+
+            i++;
+        }
+        return -1;
+    }
+
+
+    //not the best possible hash function, but will do here
+    //see here for more details:
+    //http://en.wikipedia.org/wiki/Rabin-Karp_string_search_algorithm#Hash_function_used
+    private double calculateHash(char[] s, int start, int end) {
+
+        double hash = 0;
+        for (int i = end; i >= start; i--) {
+            double addedValue = (Math.pow(10, (end - i)) * s[i]);
+            hash += addedValue;
+        }
+
+        return hash;
+    }
+
+    private double calculateRollingHash(double oldHash,
+                                        char[] s,
+                                        int addElementIndex,
+                                        int removedElementIndex,
+                                        int windowLength) {
+
+
+        double addedValue = Math.pow(3, 0) * s[addElementIndex];
+        double removedValue = Math.pow(10, windowLength - 1) * s[removedElementIndex];
+        oldHash -= removedValue;
+        oldHash *= 10;
+        return oldHash
+                + addedValue;
+    }
+
+
+}
---
+++ JavaSolutions/src/test/java/com/hackergym/samples/array/questions/SubstringTest.java
@@ -0,0 +1,149 @@
+package com.hackergym.samples.array.questions;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SubstringTest {
+
+    Substring finder =new Substring();
+    @Test
+    public void testFindSubstringBruteForce() throws Exception {
+
+        //Assert
+        String text = "It seems that I have forgotten to take a shower today";
+        String searchString = "forgotten";
+
+        //Act
+        int index = finder.findSubstringBruteForce(text, searchString);
+
+        //Assert
+        assertThat(index, is(text.indexOf(searchString)));
+    }
+
+    public void testFindSubstringBruteForceAtTheEnd() throws Exception {
+
+        //Assert
+        String text = "It seems that I have forgotten to take a shower today";
+        String searchString = "today";
+
+        //Act
+        int index = finder.findSubstringBruteForce(text, searchString);
+
+        //Assert
+        assertThat(index, is(text.indexOf(searchString)));
+    }
+
+    @Test
+    public void testFindSubstringBruteForceNonExistance() throws Exception {
+
+        //Assert
+        String text = "It seems that I have forgotten to take a shower today";
+        String searchString = "Coffee";
+
+        //Act
+        int index = finder.findSubstringBruteForce(text, searchString);
+
+        //Assert
+        assertThat(index, is(-1));
+    }
+
+    @Test
+    public void testFindSubstringBruteForceRepeatingCharacters() throws Exception {
+
+        //Assert
+        String text = "It seems that I have ppppppp to take a shower today";
+        String searchString = "ppppppp";
+
+        //Act
+        int index = finder.findSubstringBruteForce(text, searchString);
+
+        //Assert
+        assertThat(index, is(text.indexOf(searchString)));
+
+    }
+
+    @Test
+    public void testFindSubstringBruteForceLongerSubstring() throws Exception {
+
+        //Assert
+        String text = "It seems that I have forgotten to take a shower today";
+        String searchString = "Well, maybe you shouldn't have taken that pill last night";
+
+        //Act
+        int index = finder.findSubstringBruteForce(text, searchString);
+
+        //Assert
+        assertThat(index, is(-1));
+    }
+
+    @Test
+    public void testFindSubstringRabinKarp() throws Exception {
+        //Assert
+        String text = "I Have forgotten my brain";
+//        String text = "It seems that I have forgotten to take a shower today";
+        String searchString = "Hordor";
+
+        //Act
+        int index = finder.findSubstringRabinKarp(text, searchString);
+
+        //Assert
+        assertThat(index, is(text.indexOf(searchString)));
+    }
+
+
+    @Test
+    public void testFindSubstringRabinKarpAtThEnd() throws Exception {
+        //Assert
+        String text = "It seems that I have forgotten to take a shower today";
+        String searchString = "today";
+
+        //Act
+        int index = finder.findSubstringRabinKarp(text, searchString);
+
+        //Assert
+        assertThat(index, is(text.indexOf(searchString)));
+    }
+    @Test
+    public void testFindSubstringRabinKarpNonExisting() throws Exception {
+        //Assert
+        String text = "It seems that I have forgotten to take a shower today";
+        String searchString = "Coffee";
+
+        //Act
+        int index = finder.findSubstringRabinKarp(text, searchString);
+
+        //Assert
+        assertThat(index, is(-1));
+    }
+
+    @Test
+    public void testFindSubstringRabinKarpRepeatingCharacters() throws Exception {
+        //Assert
+        String text = "It seems that I have PPPPPPP to take a shower today";
+        String searchString = "PPPPPPP";
+
+        //Act
+        int index = finder.findSubstringRabinKarp(text, searchString);
+
+        //Assert
+        //Assert
+        assertThat(index, is(text.indexOf(searchString)));
+
+    }
+
+    @Test
+    public void testFindSubstringRabinKarpLongerSubstring() throws Exception {
+
+        //Assert
+        String text = "It seems that I have forgotten to take a shower today";
+        String searchString = "Well, maybe you shouldn't have taken that pill last night";
+
+        //Act
+        int index = finder.findSubstringRabinKarp(text, searchString);
+
+        //Assert
+        assertThat(index, is(-1));
+    }
+}
