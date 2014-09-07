326519f3e929dee67d67be5d84cb40fe29759a16
Ali Fathalian <fatalian@uw.edu>
2014-09-07 10:55:46 -0700

    Added Custom Solution to Problem 7 with tests

--- JavaSolutions/src/main/java/com/hackergym/samples/linkedlist/questions/Problem7.java
+++ JavaSolutions/src/main/java/com/hackergym/samples/linkedlist/questions/Problem7.java
@@ -7,7 +7,25 @@ import com.hackergym.samples.linkedlist.model.Pair;
  * Created by Fathalian on 6/8/14.
  * HackerGym.com
  */
-public class Flatten<T> {
+
+/**
+ * We have an imaginary type of linked list called Big List.
+ * A Big List is a linked list that, in addition to the ‘next’ fields, has a special field called child.
+ * The child field either points to null or points to another big list.
+ * Write an algorithm to flatten the big list.
+ *
+ * For more detailed answer see:
+ * http://hackergym.com/#/problemView/3489afb7-9dad-4310-a907-02a265bea32c?p=7
+ */
+public class Problem7<T> {
+
+    public Pair<BigListNode<T>, BigListNode<T>> flattenListCustom(BigListNode<T> head) {
+
+        //comment out the line below and add your own solution
+        //unit tests will be run automatically against your solution
+        //the tests for your custom solution start with testFlattenListCustom
+        return flattenList(head);
+    }
 
     public Pair<BigListNode<T>, BigListNode<T>> flattenList(BigListNode<T> head) {
 
--- JavaSolutions/src/test/java/com/hackergym/samples/linkedlist/questions/Problem7Test.java
+++ JavaSolutions/src/test/java/com/hackergym/samples/linkedlist/questions/Problem7Test.java
@@ -8,9 +8,9 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-public class FlattenTest {
+public class Problem7Test {
 
-    Flatten<String> solver = new Flatten<>();
+    Problem7<String> solver = new Problem7<>();
 
     @Test
     public void testFlattenList() throws Exception {
@@ -46,4 +46,37 @@ public class FlattenTest {
         }
     }
 
+    @Test
+    public void testFlattenListCustom() throws Exception {
+        Pair<BigListNode<String>, String[]> pair = LinkedListFactory.generateBigList();
+        BigListNode<String> head = pair.fst;
+        String[] expectedResult = pair.snd;
+        Pair<BigListNode<String>, BigListNode<String>> resultPair = solver.flattenListCustom(head);
+        BigListNode<String> resultHead = resultPair.fst;
+
+        for(int i = 0 ; i <expectedResult.length; i++) {
+            String expectedValue = expectedResult[i];
+            String actualValue = resultHead.value;
+            resultHead = resultHead.next;
+            assertThat(actualValue, is(expectedValue));
+        }
+
+    }
+
+    @Test
+    public void testFlattenListCustomNoChildren() throws Exception {
+
+        Pair<BigListNode<String>, String[]> pair = LinkedListFactory.generateFlatBigList(10);
+        BigListNode<String> head = pair.fst;
+        Pair<BigListNode<String>, BigListNode<String>> resultPair = solver.flattenListCustom(head);
+        BigListNode<String> resultHead = resultPair.fst;
+        String[] expectedResult = pair.snd;
+
+        for(int i = 0 ; i <expectedResult.length; i++) {
+            String expectedValue = expectedResult[i];
+            String actualValue = resultHead.value;
+            resultHead = resultHead.next;
+            assertThat(actualValue, is(expectedValue));
+        }
+    }
 }
