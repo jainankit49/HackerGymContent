7023c73ad141efe4720eb28417469b617cb6acc8
Ali Fathalian <fatalian@uw.edu>
2014-09-07 11:06:06 -0700

    Added custom solution to Problem 9 of linked list

---
+++ JavaSolutions/src/main/java/com/hackergym/samples/linkedlist/questions/Problem9.java
@@ -0,0 +1,40 @@
+package com.hackergym.samples.linkedlist.questions;
+
+import com.hackergym.samples.linkedlist.model.LinkedListNode;
+
+/**
+ * Created by Fathalian on 6/6/14.
+ * HackerGym.com
+ */
+
+/**
+ * Given access to only a single node in the middle of a linked list, remove that node.
+ * Note that you don’t have access to the head and the size of the linked list.
+ *
+ * For more detailed answer see:
+ * http://hackergym.com/#/problemView/3489afb7-9dad-4310-a907-02a265bea32c?p=9
+ */
+
+public class Problem9<T> {
+
+
+    public void straightDeleteCustom(LinkedListNode<T> nodeToDelete) {
+
+        //comment out the line below and add your own solution
+        //unit tests will be run automatically against your solution
+        //the tests for your custom solution start with testStraightDeleteCustom
+        straightDelete(nodeToDelete);
+    }
+
+    public void straightDelete(LinkedListNode<T> nodeToDelete) {
+        if (nodeToDelete == null) return;
+
+        LinkedListNode<T> current = nodeToDelete;
+        LinkedListNode<T> next = nodeToDelete.next;
+        while(next != null) {
+            current.value = next.value;
+            current = current.next;
+            next = next.next;
+        }
+    }
+}
--- JavaSolutions/src/test/java/com/hackergym/samples/linkedlist/questions/Problem9Test.java
+++ JavaSolutions/src/test/java/com/hackergym/samples/linkedlist/questions/Problem9Test.java
@@ -9,9 +9,10 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
 
-public class StraightDeleteTest {
+public class Problem9Test {
 
-    StraightDelete<String> solver = new StraightDelete<>();
+    Problem9<String> solver = new Problem9<>();
+
     @Test
     public void testStraightDelete() throws Exception {
 
@@ -42,6 +43,36 @@ public class StraightDeleteTest {
         testDelete(head, 0);
     }
 
+    @Test
+    public void testStraightDeleteCustom() throws Exception {
+
+        LinkedListNode<String> head = LinkedListFactory.generateStringLinkedList(7);
+        testDeleteCustom(head, 3);
+    }
+
+    @Test
+    public void testStraightDeleteCustomLastElement() throws Exception {
+
+        //note that the algorithm won't work for the last element.
+        LinkedListNode<String> head = LinkedListFactory.generateStringLinkedList(7);
+
+        //use basic linked list helper to get the node to delete
+        BasicLinkedList<String> linkedListHelper = new BasicLinkedList<>(head);
+        LinkedListNode<String> nodeToDelete = linkedListHelper.getNodeAtIndex(6);
+        String valueToDelete = nodeToDelete.value;
+        solver.straightDeleteCustom(nodeToDelete);
+
+        LinkedListNode<String> deletedNode = linkedListHelper.searchForValue(valueToDelete);
+        assertThat(deletedNode, is(notNullValue()));
+    }
+
+    @Test
+    public void testStraightDeleteFirstElement() throws Exception {
+
+        LinkedListNode<String> head = LinkedListFactory.generateStringLinkedList(7);
+        testDeleteCustom(head, 0);
+    }
+
     private void testDelete(LinkedListNode<String> head, int indexToDelete) {
 
         //use basic linked list helper to get the node to delete
@@ -49,6 +80,19 @@ public class StraightDeleteTest {
         LinkedListNode<String> nodeToDelete = linkedListHelper.getNodeAtIndex(indexToDelete);
         String valueToDelete = nodeToDelete.value;
         solver.straightDelete(nodeToDelete);
+
+        LinkedListNode<String> deletedNode = linkedListHelper.searchForValue(valueToDelete);
+        assertThat(deletedNode, is(nullValue()));
+
+    }
+
+    private void testDeleteCustom(LinkedListNode<String> head, int indexToDelete) {
+
+        //use basic linked list helper to get the node to delete
+        BasicLinkedList<String> linkedListHelper = new BasicLinkedList<>(head);
+        LinkedListNode<String> nodeToDelete = linkedListHelper.getNodeAtIndex(indexToDelete);
+        String valueToDelete = nodeToDelete.value;
+        solver.straightDeleteCustom(nodeToDelete);
 
         LinkedListNode<String> deletedNode = linkedListHelper.searchForValue(valueToDelete);
         assertThat(deletedNode, is(nullValue()));
